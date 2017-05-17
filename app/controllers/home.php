bc98458dcd37ffa6c7fbbd302a960253fb52b136
Nana Adjei Manu <claeusdev@gmail.com>
2017-05-17 13:38:16 +0000

    Initial commit with basic routing

---
+++ app/controllers/home.php
@@ -0,0 +1,23 @@
+<?php
+
+
+/**
+ *
+ */
+class Home extends Controller
+{
+
+  function __construct()
+  {
+    # code...
+  }
+
+
+  public function index()
+  {
+    echo 'home/index';
+  }
+}
+
+
+ ?>
